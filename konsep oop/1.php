d8bf8459eda61f66e0e56a6830a18c010a3450d0
revan933 <smkiu.010@gmail.com>
2025-07-23 21:59:27 +0700

    belajar konsep oop

---
+++ konsep oop/1.php
@@ -0,0 +1,17 @@
+<php?
+class Product
+{
+    public $name;
+    public $price;
+    public $description;
+
+    public function _construct($name, $price, $description)
+    {
+        $this->name = $name;
+        $this->price = $price;
+        $this->description = $description;
+    }
+
+}
+
+$product1 = new Product('Smartphone', 5000000,'Smartphone terbaru dengan kamera berkualitas tinggi');
